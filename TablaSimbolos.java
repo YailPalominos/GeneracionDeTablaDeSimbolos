e9346c3ea18aea901ad95a5a8fa3519485d8f46b
Jaip3rr <129027070+Jaip3rr@users.noreply.github.com>
2023-09-01 11:14:26 -0600

    Actualizacion

--- TablaSimbolos.java
+++ TablaSimbolos.java
@@ -1,19 +1,62 @@
 public class TablaSimbolos {
 
-    public String Token;
-    public String Tipo;
-    public int IdToken;
-    public int Repeticiones;
-    public String Linea;
-    public float Valor;
+    private String token;
+    private String tipo;
+    private int idToken;
+    private int repeticiones;
+    private String linea;
+    private float valor;
 
     public TablaSimbolos() {
-        Token = "";
-        Tipo = "";
-        IdToken = 0;
-        Repeticiones = 0;
-        Linea = "";
-        Valor = 0;
+       
+    }
+
+    public void setToken( String token ){
+        this.token=token;
+    }
+
+    public String getToken(){
+        return token;
+    }
+
+    public void setTipo( String tipo ){
+        this.tipo=tipo;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+
+    public void setIdToken( int idToken ){
+        this.idToken=idToken;
+    }
+
+    public int getIdToken(){
+        return idToken;
+    }
+
+    public int getRepeticiones() {
+        return repeticiones;
+    }
+
+    public void setRepeticiones(int repeticiones) {
+        this.repeticiones = repeticiones;
+    }
+ 
+    public String getLinea() {
+        return linea;
+    }
+
+    public void setLinea(String linea) {
+        this.linea = linea;
+    }
+ 
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
     }
 
 }
