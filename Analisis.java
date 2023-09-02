83177bd5e7616e22ec2cbaad1df2e72f2ec04ab4
YailPalominos <yail.palominos@gmail.com>
2023-09-01 22:34:49 -0600

    Revert "UPDATE"
    This reverts commit e07dd7b7d3da0d368f57fbc8910caef149100bdb.

--- Analisis.java
+++ Analisis.java
@@ -1,130 +1,151 @@
-import java.util.HashSet;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class Analisis {
 
-    String sCodigoFuente = "";
-    
-    TablaSimbolos[] TaSimbolos = new TablaSimbolos[0];
-    
+    /**
+     * @author Braulio Yail Palominos Patiño
+     * @co Author Jose Adrian Terrones Perez
+     */
 
-    Resultado oResultado = new Resultado();
-    int nPosLectura = 0;
-    int nLinea = 0;
-    Set<String> simbolosAgregados = new HashSet<>();
-    Set<String> PRAgregados = new HashSet<>();
+    /*
+     * Ojo
+     * Tomar en cuenta que las llaves alteran la posicion de los grupos del marcher
+     * En caso de agregar mas grupos
+     */
+
+    /*
+     * Estas son las expreciones regulares que utiliza generalmente el analizador
+     * lexico
+     *
+     * Palabras reservadas
+     * (class|for|if|float|int|boolean|static|new|static|void|int|string|import|
+     * public|else|programa|binario)|"
+     * + "Identificadores ([a-zA-Z]+)|"
+     * + "Relacionales ([<|>]+)|"
+     * + "Aritmeticas ([+]|[-])|"
+     * + "Asignacion([=]+)|"
+     * + "Parentesis ([(|)]+)|"
+     * + "LLaves ([{|}]+)|"
+     * + "Punto y coma(;)|"
+     * + "Binario ([0-1]b)|"
+     * + "Octal ([0-8]o)|"
+     * + "Hexadecimal (^[0-9A-F]+$)|"
+     * + "Espacios (^(\s)*)";
+     */
+
+    String sCodigoFuente = ""; // Codigo fuente resibido
+    String sCodigoFuenteErrores = ""; // Codigo fuente resibido
+    // de analisis
+    String[] aFiguras = new String[0]; // Cadena de las figuras a analizar
+    Resultado oResultado = new Resultado(); // Clase para imprimir los resultados
+    // Validacion oDesglosar = new Validacion(); // Clase para verificar la cadena
+    // de figura
+    int nPosLectura = 0; // posicion de lectura con respecto al codigo fuente
+    int nLinea = 0; // Linea en la que va la posicion de lectura con respecto al codigo fuente
+
+    TablaSimbolos[] TaToken = new TablaSimbolos[0];
 
     public Analisis(String sCodigoFuente) {
         this.sCodigoFuente = sCodigoFuente;
-        
+        this.sCodigoFuenteErrores = sCodigoFuente;
     }
 
+    // Genera las clases con los simbolos
     public void Generar() {
 
-        System.out.println("hola");
-        // Establecemos una cadena de coincidencias Esto es una expresion regular
-        String coincidencias = "(Linea|Circulo|Triangulo|Cuadrado|Rectangulo)\\b|"
-        + "([0-9]+)|"
-        + "(\\s+)|"
-        + "^(\\s*)";
+        // Codigo fuente a chart para leer parte por parte
+        var cLetras = this.sCodigoFuenteErrores.toCharArray();
+        String sPalabra = "";
+        Pattern pPatronError = Pattern.compile(
+                "(((\\(\\d{1,2},\\d{1,2}\\)){2,4})+-+\\([1-3]\\)+-+\\((25[0-5]|2[0-4]\\d|1\\d{1,2}|\\d{1,2}),(25[0-5]|2[0-4]\\d|1\\d{1,2}|\\d{1,2}),(25[0-5]|2[0-4]\\d|1\\d{1,2}|\\d{1,2})\\))");
+        this.nPosLectura = 0;
+        // Recorre palabra por palabra encontrada
+        for (int y = 0; y < cLetras.length; y++) {
 
-        // Define un patron de busquedas dentro de nuestra cadena de coincidencias
-        Pattern pPatron = Pattern.compile(coincidencias);
-        // ralizara la búsqueda de nuestra coincidencias
-        Matcher mMatcher = pPatron.matcher(sCodigoFuente);
+            nLinea = ObtenerLinea(sCodigoFuenteErrores, y);
+            sPalabra += cLetras[y];
+            this.nPosLectura++;
 
-        // Buscamos las coincidencias con el ciclo While
-        int idToken = 0;
-        
-        while (mMatcher.find()) {
+            if (sPalabra.split("\\s").length > 0) {
+                this.nPosLectura -= 1;
+                sPalabra = sPalabra.trim();
 
-            String tokenPalabrasrReservadas = mMatcher.group(1);
-            String tokenDigito = mMatcher.group(2);
-            String tokenEspacios = mMatcher.group(3);
-            
-            int nPosInicioLexema = 0;
-            int repeticiones = 1;
-            nLinea = ObtenerLinea(sCodigoFuente, mMatcher.start());
+                char cLeta = cLetras[y];
+                var x = ((cLeta + "").replace("", " ").trim());
 
-            //Palabras Reservadas
-            if (tokenPalabrasrReservadas != null) {
+                if (sPalabra.equals("Rectangulo")) {
+                    sPalabra = sPalabra.replaceAll("Rectangulo", "");
+                    nPosLectura += "Rectangulo".length();
+                }
 
-                nPosLectura += tokenPalabrasrReservadas.length();
-                nPosInicioLexema = nPosLectura - tokenPalabrasrReservadas.length();
-                idToken++;
-                AgregarTablaSimbolos(tokenPalabrasrReservadas, "String", idToken, repeticiones, String.valueOf(nLinea), 0f);
-                
-            if (!PRAgregados.contains(tokenPalabrasrReservadas))
-                PRAgregados.add(tokenPalabrasrReservadas);
+                if (sPalabra.equals("Triangulo")) {
+                    sPalabra = sPalabra.replaceAll("Triangulo", "");
+                    nPosLectura += "Triangulo".length();
+                }
+
+                if (sPalabra.equals("Cuadrado")) {
+                    sPalabra = sPalabra.replaceAll("Cuadrado", "");
+                    nPosLectura += "Cuadrado".length();
+                }
+                if (sPalabra.equals("Circulo")) {
+                    sPalabra = sPalabra.replaceAll("Circulo", "");
+                    nPosLectura += "Circulo".length();
+                }
+                if (sPalabra.equals("Linea")) {
+                    sPalabra = sPalabra.replaceAll("Linea", "");
+                    nPosLectura += "Linea".length();
+                }
+
+                if (x.length() == 0) {
+                    Matcher mMatcherError = pPatronError.matcher(sPalabra);
+
+                    if (mMatcherError.find()) {
+                        sPalabra = "";
+                    } else {
+                        this.nPosLectura += sPalabra.length();
+                        int nPosInicioLexema = this.nPosLectura - sPalabra.length();
+                        System.out.println();
+                        System.out.format("%10s %10s %10s %10s",
+                                " \033[31mERROR léxico:  \033[0m" + sPalabra, " Linea " + nLinea,
+                                " Inicia " + nPosInicioLexema, " Termina " + nPosLectura);
+                        System.out.println();
+                        sPalabra = "";
+                    }
+                }
             }
-
-
-            //Digito
-            if(tokenDigito != null){
-                nPosLectura += tokenDigito.length();
-                nPosInicioLexema = nPosLectura - tokenDigito.length();
-                idToken++;
-                float valor = Float.parseFloat(tokenDigito);
-                String tipo = (Math.floor(valor) == valor) ? "Int" : "Float";
-                AgregarTablaSimbolos(tokenDigito, tipo, idToken, repeticiones, String.valueOf(nLinea), Float.parseFloat(tokenDigito));
-            }
-
-           //Espacios
-            if (tokenEspacios != null) {
-                nPosLectura += tokenEspacios.length();
-                nPosInicioLexema = nPosLectura - tokenEspacios.length();
-            }
-            
-
         }
 
-        
-        oResultado.ImprimirTblaSimb(TaSimbolos);
+        this.oResultado.ImprimirTblaTokens(TaToken);
 
     }
 
-    
-    /**
-     * Metodo para agregar un simbolo a la Tabla de simbolos
-     * @param sSimbolo
-     * @param sLexema
-     * @param sLinea
-     * @param sPosInicioLexema
-     * @param sPosFinalLexema
-     */
-    public void AgregarTablaSimbolos(String sToken, String sTipo, int sIdToken, int sRepeticiones, String sLinea, float sValor) {
-    // Verificamos si el token ya se encuentra en la tabla de símbolos
-    for (int i = 0; i < this.TaSimbolos.length; i++) {
-        if (this.TaSimbolos[i].getToken().equals(sToken)) {
-            this.TaSimbolos[i].setRepeticiones(this.TaSimbolos[i].getRepeticiones() + 1);
-            // Si el token ya existe, concatenamos las líneas
-            this.TaSimbolos[i].setLinea(this.TaSimbolos[i].getLinea() + ", " + sLinea);
-            return; // Salimos del método, no agregamos un nuevo registro
-        }
+    public void AgregarTablaToken(String sToken, String sLexema, int nLinea, int nPosInicioLexema,
+            int nPosFinalLexema) {
+
+        TablaSimbolos[] aTblaTokensNueva = this.TaToken;
+        aTblaTokensNueva = new TablaSimbolos[this.TaToken.length + 1];
+        System.arraycopy(this.TaToken, 0, aTblaTokensNueva, 0, this.TaToken.length);
+
+        // TablaSimbolos oTblaSimbolo = new TablaSimbolos();
+        // oTblaSimbolo.simbolo = sToken;
+        // oTblaSimbolo.lexema = sLexema;
+        // oTblaSimbolo.linea = nLinea;
+        // oTblaSimbolo.posInicioSimbolo = nPosInicioLexema;
+        // oTblaSimbolo.posFinalSimbolo = nPosFinalLexema;
+        // oTblaSimbolo.posFinalSimbolo = nPosFinalLexema;
+
+        // if (oTblaSimbolo.simbolo.contains("Figura:")) {
+        // AgregarFigura(oTblaSimbolo.lexema);
+        // }
+
+        // aTblaTokensNueva[aTblaTokensNueva.length - 1] = oTblaSimbolo;
+        this.TaToken = aTblaTokensNueva;
+
     }
-
-    // Si el token no existe en la tabla, entonces lo agregamos como un nuevo registro
-    TablaSimbolos[] aSimboloNuevo = new TablaSimbolos[this.TaSimbolos.length + 1];
-    System.arraycopy(this.TaSimbolos, 0, aSimboloNuevo, 0, this.TaSimbolos.length);
-
-    TablaSimbolos oSimbolo = new TablaSimbolos();
-    oSimbolo.setToken(sToken);
-    oSimbolo.setTipo(sTipo);
-    oSimbolo.setIdToken(sIdToken);
-    oSimbolo.setRepeticiones(sRepeticiones);
-    oSimbolo.setLinea(sLinea);
-    oSimbolo.setValor(sValor);
-
-    aSimboloNuevo[aSimboloNuevo.length - 1] = oSimbolo;
-
-    this.TaSimbolos = aSimboloNuevo;
-}//Fin del metodo AgregarTablaSimbolos
 
     int ObtenerLinea(String sCodigoFuente, int nInicio) {
-        int nLinea = 1;
+        int nLinea = 0;
         Pattern pPatron = Pattern.compile("\n");
         Matcher mMatcher = pPatron.matcher(sCodigoFuente);
         mMatcher.region(0, nInicio);
@@ -132,6 +153,12 @@ public class Analisis {
         while (mMatcher.find()) {
             nLinea++;
         }
+
+        if (this.nLinea != nLinea) {
+            nPosLectura = 0;
+        }
+
         return (nLinea);
     }
+
 }
--- Resultado.java
+++ Resultado.java
@@ -3,29 +3,30 @@ public class Resultado {
         public Resultado() {
         }
 
-        public void ImprimirTblaSimb(TablaSimbolos[] oSimbolos) {
+        public void ImprimirTblaTokens(TablaSimbolos[] oSimbolos) {
                 System.out.println();
                 System.out.println(
                                 "---------------------------------------------------------------------------------------------------------------------------------");
-                System.out.printf("%70s", "Tabla de Simbolos");
+                System.out.printf("%70s", "Tabla de Tokens");
                 System.out.println();
                 System.out.println(
                                 "---------------------------------------------------------------------------------------------------------------------------------");
-                System.out.printf("%15s %15s %15s %15s %15s %15s", "Token", "Tipo", "id_Token", "Repeticiones", "Linea", "Valor", "");
+                System.out.printf("%10s %10s %10s %10s %10s", "Token", "Lexema", "Linea", "Inicio", "Final");
                 System.out.println();
                 System.out.println(
                                 "---------------------------------------------------------------------------------------------------------------------------------");
 
                 for (int x = 0; x < oSimbolos.length; x++) {
-                        TablaSimbolos Simbolo = oSimbolos[x];
+                        var Simbolo = oSimbolos[x];
 
-                        System.out.format("%15s %15s %15s %15s %15s %15s", Simbolo.getToken(), Simbolo.getTipo(), Simbolo.getIdToken(),
-                                        Simbolo.getRepeticiones(), Simbolo.getLinea(), Simbolo.getValor());
+                        System.out.format("%10s %10s %10s %10s %10s %10s", Simbolo.Token, Simbolo.Tipo, Simbolo.IdToken,
+                                        Simbolo.Repeticiones, Simbolo.Linea, Simbolo.Valor);
                         System.out.println();
 
                 }
                 System.out.println(
                                 "---------------------------------------------------------------------------------------------------------------------------------");
+
         }
 
 }
